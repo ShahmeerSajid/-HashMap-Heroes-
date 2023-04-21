3b91027372d141f69dd5e66bda0b04e698a67cc6
ShahmeerSajid <shahmeersajid732@gmail.com>
2023-04-21 19:44:28 -0400

    finish genderbykeyword

--- finalproject/GenderByKeyword.java
+++ finalproject/GenderByKeyword.java
@@ -5,21 +5,78 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class GenderByKeyword extends DataAnalyzer {
-	
+	// 3 hash maps store String, Integer
+	private MyHashTable<String, Integer> M_Hashmap;
+	private MyHashTable<String, Integer> W_Hashmap;
+	private MyHashTable<String, Integer> X_Hashmap;
+
 	public GenderByKeyword(Parser p) {
 		super(p);
 	}
 
 	@Override
 	public MyHashTable<String, Integer> getDistByKeyword(String keyword) {
-		// ADD YOUR CODE BELOW THIS
-		return null;
-		//ADD YOUR CODE ABOVE THIS
+		MyHashTable<String, Integer> result_hashmap = new MyHashTable<>(4);
+
+		Integer M_count = M_Hashmap.get(keyword);
+		if (M_count == null) {
+			M_count = 0;
+		}
+		result_hashmap.put("M", M_count);
+
+		Integer W_count = W_Hashmap.get(keyword);
+		if (W_count == null) {
+			W_count = 0;
+		}
+		result_hashmap.put("F", W_count);
+
+		Integer X_count = X_Hashmap.get(keyword);
+		if (X_count == null) {
+			X_count = 0;
+		}
+		result_hashmap.put("X", X_count);
+
+		return result_hashmap;
 	}
 
 	@Override
 	public void extractInformation() {
+		M_Hashmap = new MyHashTable<>();
+		W_Hashmap = new MyHashTable<>();
+		X_Hashmap = new MyHashTable<>();
 
+		for (int i = 0; i < parser.data.size(); i++) {
+			String[] data = parser.data.get(i);
+			String gender = data[7];
+			MyHashTable<String, Integer> current_Hashmap;
+			if (gender.equals("M")){
+				current_Hashmap = M_Hashmap;
+			} else if (gender.equals("F")) {
+				current_Hashmap = W_Hashmap;
+			} else {
+				current_Hashmap = X_Hashmap;
+			}
+
+			String comments = data[6].toLowerCase();
+			String output = comments.replaceAll("[^a-zA-Z']", " ");
+			String[] word_arr = output.split("\\s+");
+			for (String word : word_arr) {
+				Integer count = current_Hashmap.get(word);
+				if (count == null) {
+					current_Hashmap.put(word, 1);
+				} else {
+					current_Hashmap.put(word, count + 1);
+				}
+			}
+		}
+//		MyHashTable<String, Integer> res1 = getDistByKeyword("caring");
+//		for (String key : res1.getKeySet()) {
+//			System.out.println(key + ": " + res1.get(key));
+//		}
+//		res1 = getDistByKeyword("smart");
+//		for (String key : res1.getKeySet()) {
+//			System.out.println(key + ": " + res1.get(key));
+//		}
 	}
 
 }
--- finalproject/RatingByKeyword.java
+++ finalproject/RatingByKeyword.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class RatingByKeyword extends DataAnalyzer {
-	
+
     public RatingByKeyword(Parser p) {
         super(p);
     }
@@ -12,6 +12,9 @@ public class RatingByKeyword extends DataAnalyzer {
 	@Override
 	public MyHashTable<String, Integer> getDistByKeyword(String keyword) {
 		// ADD YOUR CODE BELOW THIS
+		// same as gender
+		// strongly consider making helper that takes map and returns count
+
 		return null;
 		//ADD YOUR CODE ABOVE THIS
 	}
@@ -19,6 +22,17 @@ public class RatingByKeyword extends DataAnalyzer {
 	@Override
 	public void extractInformation() {
 		// ADD YOUR CODE BELOW THIS
+
+		// 5 hash maps for each rating number of String, Integer
+		// String = keyword, Integer = num reviews
+
+		// my_arraylist
+		// for word in word_arr
+		// !my_arraylist.contains(word) => add
+
+		// for word in my_arraylist
+		// determine hash map from review rating => rounding down
+		// add to hashmap (1 if not exists, count + 1 otherwise)
 
 		//ADD YOUR CODE ABOVE THIS
 	}
